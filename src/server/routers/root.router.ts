f50ecccf157041b80aa09f1415a5237d42fca48e
Paul Lessing <paul@paullessing.com>
2017-01-31 22:51:48 +0000

    Merge server and home servers

--- src/server/routers/root.router.ts
+++ src/server/routers/root.router.ts
@@ -2,16 +2,22 @@ import {Get, Response, Use, UseType} from 'express-router-decorators';
 
 import {Service} from '../../util';
 import {DeviceRouter} from './devices.router';
+import {AlexaRouter} from './alexa.router';
 
 @Service()
 export class RootRouter {
+
+  @Use('/alexa', UseType.ROUTER)
+  public alexaRouter: AlexaRouter;
 
   @Use('/devices', UseType.ROUTER)
   public deviceRouter: DeviceRouter;
 
   constructor(
+    alexaRouter: AlexaRouter,
     deviceRouter: DeviceRouter
   ) {
+    this.alexaRouter = alexaRouter;
     this.deviceRouter = deviceRouter;
   }
 
--- src/web-server/server.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata"; // Required for inversify
-
-import {app} from './app';
-
-const port = 8081;
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
